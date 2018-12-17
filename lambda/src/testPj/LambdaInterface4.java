a3ac54f4350aa3a7184879955c0ce710e5e96f1f
madforre <33324825+madforre@users.noreply.github.com>
2018-12-18 02:35:19 +0900

    lambda +

--- lambda/src/testPj/LambdaInterface4.java
+++ lambda/src/testPj/LambdaInterface4.java
@@ -2,7 +2,7 @@ package testPj;
 
 public interface LambdaInterface4 {
 
-    // 반환 값이 있는 경우
+    // 반환 값이 있는 경우 데이터 값 명시
     public int method(int x, int y);
     
 }
--- lambda/src/testPj/MainClass.java
+++ lambda/src/testPj/MainClass.java
@@ -27,19 +27,20 @@ public class MainClass {
         li5.method();
 
         // 반환 값이 있는 경우
-        LambdaInterface4 li6 = (x, y) -> {
+        
+        LambdaInterface4 li6 = (x, y) -> { // 매개변수의 타입이 같으므로 생략해주었고, 실행문이 2개이므로 중괄호는 생략하지 않고 사용하였따.
             int result = x + y;
             return result;
         };
         System.out.printf("li6.method(10, 20) : %d\n", li6.method(10, 20));
         
-        li6 = (x, y) - > {
+        li6 = (x, y) - > { // 람다식을 다시 사용, 익명함수로 익명 객체를 생성하여 원하는 대로 유연하게 메서드를 바꿔줄 수 있다. (곱셈)
             int result = x * y;
             return result;
         };
         System.out.printf("li6.method(10, 20) : %d\n", li6.method(10, 20));
 
-        li6 = (x, y) -> {
+        li6 = (x, y) -> { // 뺄셈으로 메서드를 다시 변경해준 모습
             int result = x - y;
             return result;
         };
